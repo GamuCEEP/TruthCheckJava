8f3cf4f129d2c8cc0b68bd9e15c06f79cac0b502
GamuCEEP <victordiez.ceep@gmail.com>
2022-03-04 09:59:33 +0100

    Que bonitas las templates

--- src/main/webapp/components/modal/modal.js
+++ src/main/webapp/components/modal/modal.js
@@ -9,41 +9,43 @@
 // <div> modal </div>
 
 const pathToModals = '/TruthCheckJava/pages/modals/'
+let cover;
+; (() => {
+  if (!document.head.querySelector('link[href~="modal.css"')) {
+    const css = document.createElement('link')
+    css.setAttribute('rel', 'stylesheet')
+    css.setAttribute('href', '/TruthCheckJava/components/modal/modal.css')
+    document.head.append(css)
+  }
+})()
 
-  ; (() => {
-    if(!document.head.querySelector('link[href~="modal.css"')){
-      const css = document.createElement('link')
-      css.setAttribute('rel', 'stylesheet')
-      css.setAttribute('href','/TruthCheckJava/components/modal/modal.css')
-      document.head.append(css)
-    }
-  })()
-
-function attributeListToObj(attrList){
+function attributeListToObj(attrList) {
   let res = {}
-  for(const attr of attrList){
+  for (const attr of attrList) {
     res[attr.name] = attr.value
   }
   return res
 }
 
 
-async function setModalButtons() {
+async function setModalButtons(from) {
 
   const modalContainer = $('#modalContainer')
-  modalContainer.innerHTML = ''
+  const modalBox = document.createElement('div')
+  modalContainer.append(modalBox)
+  modalBox.innerHTML = ''
+  console.log(from)
 
-  const cover = createCover()
-  modalContainer.append(cover)
-
-  const buttons = $$('.modal-button')
+  const buttons = $$('.modal-button', from)
   for (const button of buttons) {
     if (!button.hasAttribute('modal')) continue
 
     const modal = await getModal(button)
-    modalContainer.append(modal)
+    modalBox.append(modal)
+    evaluate(modal)
 
     function showModal() {
+      hideModals()
       cover.removeAttribute('style')
       modal.removeAttribute('style')
     }
@@ -54,21 +56,24 @@ async function setModalButtons() {
   }
 }
 
+cover = createCover()
 setModalButtons()
 
-function createCover(){
+function createCover() {
   const cover = document.createElement('div')
   cover.classList.add('modal-cover')
   cover.setAttribute('style', 'display: none')
-  // eventListener para ocultar el modal cuando cliques
-  // Tiene que ser una funcion separada para poder llamarla
-  // desede un boton normal
+
+  const modalContainer = $('#modalContainer')
+  modalContainer.append(cover)
+  cover.addEventListener('click', () => {
+    hideModals()
+  })
   return cover
 }
 
-async function getModal(button){
+async function getModal(button) {
   const modalURL = pathToModals + button.getAttribute('modal') + '.template.html'
-
   const attributes = attributeListToObj(button.attributes)
 
   const modalTemplate = await getTemplate(modalURL, attributes)
@@ -77,6 +82,21 @@ async function getModal(button){
   modal.setAttribute('style', 'display: none')
   modal.classList.add('modal')
   return modal
+}
+
+function hideModals() {
+  for (const modal of $$('.modal', modalContainer)) {
+    modal.setAttribute('style', 'display: none')
+  }
+  $('.modal-cover').setAttribute('style', 'display: none')
+}
+
+async function evaluate(modal) {
+  const scripts = $$('script', modal)
+  for (const script of scripts) {
+    console.log(modal, script)
+    await eval(script.innerText)
+  }
 }
 
 
